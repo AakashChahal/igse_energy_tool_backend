41d33cc8b3467c2786755870cabfc65260d7c494
Aakash Chahal <aakash.chahal62@gmail.com>
2022-12-21 15:10:16 +0000

    getting the GET and POST requests

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const firebase = require("firebase/app");
 const app = express();
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAF57fDJpxBjL2OS4R1x7vHNxGOaugdOeY",
+    authDomain: "igse-energy-tool.firebaseapp.com",
+    databaseURL: "https://igse-energy-tool-default-rtdb.firebaseio.com",
+    projectId: "igse-energy-tool",
+    storageBucket: "igse-energy-tool.appspot.com",
+    messagingSenderId: "994278000336",
+    appId: "1:994278000336:web:7099deec2acecbd2ee80d5",
+    measurementId: "G-YGQ5LCHR41",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 app.use(cors());
 app.use(express.json());
@@ -11,9 +25,33 @@ app.get("/", (req, res) => {
     res.redirect("/login");
 });
 
-app.get("/login", (req, res) => {
+app.post("/login", (req, res) => {
+    console.log(req);
     res.send({
         message: "Hello World!",
+        status: 200,
+    });
+});
+
+app.get("/login", (req, res) => {
+    res.send({
+        message: "Login Page",
+        status: 200,
+    });
+});
+
+app.get("/register", (req, res) => {
+    res.send({
+        message: "Register Page",
+        status: 200,
+    });
+});
+
+app.post("/register", (req, res) => {
+    console.log(req);
+    res.send({
+        message: "Registration Successfull",
+        status: 200,
     });
 });
 
